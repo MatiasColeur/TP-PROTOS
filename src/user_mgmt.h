c3b486b3c28a6cbd0e1a72d910d09c556a37b721
valenNovillo <130627694+valenNovillo@users.noreply.github.com>
2025-12-10 23:43:04 -0300

    adding user mgmt library (add, delete, modify user role)

---
+++ src/user_mgmt.h
@@ -0,0 +1,19 @@
+// user_store.h
+#pragma once
+#include <stdbool.h>
+#include <stddef.h>
+
+typedef struct {
+    char user[64];
+    char pass_hash[128];   
+    char role[32];
+} user_record;
+
+bool user_store_load(const char *csv_path);                
+bool user_store_save(const char *csv_path);                
+bool user_store_add(const user_record *rec);               
+bool user_store_update(const char *user, const user_record *rec); 
+bool user_store_delete(const char *user);                  
+const user_record *user_store_find(const char *user);     
+size_t user_store_list(user_record *out, size_t max_out); 
+void user_store_close(void);                               
